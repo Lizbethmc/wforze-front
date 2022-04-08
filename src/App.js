5c64fce530550015a7036edb7a6339a9a374c962
Flor-AC <florac206@gmial.com>
2022-04-07 21:42:22 -0500

    Add register

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-import {InputText} from 'primereact/inputtext';
-
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
+import registrar from 'Register/register';
+import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            <InputText/>
-            </header>
+            <register />
         </div>
     );
 }
